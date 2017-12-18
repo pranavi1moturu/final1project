4febfda71460d3f696554c221cc81d8938269d44
pranavi moturu <pm487@afsconnect1.njit.edu>
2017-12-17 22:20:11 -0500

    madesomechanges

--- database.php
+++ database.php
@@ -1,6 +1,44 @@
 <?php
-
-define('DATABASE', 'kwilliam');
-define('USERNAME', 'kwilliam');
-define('PASSWORD', 'USracpEto');
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+define('DATABASE', 'pm487');
+define('USERNAME', 'pm487');
+define('PASSWORD', 'kKmilXOt');
 define('CONNECTION', 'sql2.njit.edu');
+class dbConn
+{
+    //variable to hold connection object.
+       protected static $db;
+   
+      private function __construct() 
+        {
+
+           try 
+             {
+              
+              self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
+              self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+             }
+
+           catch (PDOException $e)
+             {
+              //error when connection is not established
+              echo "Connection Error: " . $e->getMessage();
+             }
+        }
+    
+    
+      public static function getConnection() 
+        {
+           //if there is no active connection, create a new connection
+            if (!self::$db) 
+            {
+                //new connection.
+                 new dbConn();
+            }
+        
+          //returning new  connection.
+          return self::$db;
+        }
+}
+?>
